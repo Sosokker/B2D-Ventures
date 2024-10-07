f64ac32b400ed0fe2e0b045742295dbd155940bd
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-10-08 05:18:40 +0700

    Refactor component and page structure for responsiveness and add remote image patterns

--- src/app/business/apply/page.tsx
+++ src/app/business/apply/page.tsx
@@ -1,7 +1,23 @@
-"use client"
-
-export default function ApplyPage(){
-    return (
-        <div></div>
-    );
+export default function Apply() {
+  return (
+    <div>
+      <div>
+        <div className="grid grid-flow-row auto-rows-max w-full backdrop-blur h-60 md:h-96 bg-gray-100 dark:bg-gray-800 p-5">
+          <h1 className="text-xl md:text-5xl font-medium md:font-bold justify-self-center mt-10 md:mt-28">
+            Apply to raise on B2DVentures
+          </h1>
+          <div className="mt-10 justify-self-center">
+            <p className="text-sm md:text-base">
+              All information submitted in this application is for internal use
+              only and is treated with the utmost{" "}
+            </p>
+            <p className="mt-2 text-sm md:text-base">
+              confidentiality. Companies may apply to raise with B2DVentures
+              more than once.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
