0e1b6c95354245c684b0ff237c238029c7d05db4
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-10-08 15:38:11 +0700

    Refactor component and page structure for responsiveness and add remote image patterns

--- src/app/business/apply/page.tsx
+++ src/app/business/apply/page.tsx
@@ -1,21 +1,36 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Apply() {
   return (
     <div>
-      <div>
-        <div className="grid grid-flow-row auto-rows-max w-full backdrop-blur h-60 md:h-96 bg-gray-100 dark:bg-gray-800 p-5">
-          <h1 className="text-xl md:text-5xl font-medium md:font-bold justify-self-center mt-10 md:mt-28">
-            Apply to raise on B2DVentures
-          </h1>
-          <div className="mt-10 justify-self-center">
-            <p className="text-sm md:text-base">
-              All information submitted in this application is for internal use
-              only and is treated with the utmost{" "}
-            </p>
-            <p className="mt-2 text-sm md:text-base">
-              confidentiality. Companies may apply to raise with B2DVentures
-              more than once.
-            </p>
-          </div>
+      <div className="grid grid-flow-row auto-rows-max w-full h-52 md:h-92 bg-gray-100 dark:bg-gray-800 p-5">
+        <h1 className="text-2xl md:text-5xl font-medium md:font-bold justify-self-center md:mt-8">
+          Apply to raise on B2DVentures
+        </h1>
+        <div className="mt-5 justify-self-center">
+          <p className="text-sm md:text-base text-neutral-500">
+            All information submitted in this application is for internal use
+            only and is treated with the utmost{" "}
+          </p>
+          <p className="text-sm md:text-base text-neutral-500">
+            confidentiality. Companies may apply to raise with B2DVentures more
+            than once.
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-flow-row auto-rows-max w-full">
+        <h1 className="text-3xl font-bold mt-10 ml-96">About your company</h1>
+        <p className="ml-96 mt-5 text-neutral-500">
+          All requested information in this section is required.
+        </p>
+        {/* form */}
+        <div className="ml-96 mt-5">
+          <Label htmlFor="companyName" className="font-bold text-lg">
+            Company name
+          </Label>
+          <Input type="email" id="companyName" className="mt-2 w-96" />
         </div>
       </div>
     </div>
