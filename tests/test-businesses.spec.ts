445836fa58e48fdc17264c4533cccacd788528b5
sirin <sirinza29@gmail.com>
2024-10-28 01:01:22 +0700

    fix: fail test-businesses

--- tests/test-businesses.spec.ts
+++ tests/test-businesses.spec.ts
@@ -1,23 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 test.use({
-  storageState: './storageState.json'
+  storageState: "./storageState.json",
 });
 
-test('Test search businesses', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/');
-  await page.getByLabel('Main').getByRole('img').click();
+test("Test search businesses", async ({ page }) => {
+  await page.goto("http://127.0.0.1:3000/");
+  await page.getByLabel("Main").getByRole("img").click();
 
-  const businessInput = page.getByPlaceholder('Enter business name...');
+  const businessInput = page.getByPlaceholder("Enter business name...");
   await expect(businessInput).toBeVisible();
-  await businessInput.fill('neon');
-  await businessInput.press('Enter');
+  await businessInput.fill("neon");
+  await businessInput.press("Enter");
 
-  const heading = page.getByRole('heading', { name: 'Neon Solution, A dummy company' });
+  const heading = page.getByRole("heading", { name: "Neon Solution, A dummy company" });
   await expect(heading).toBeVisible();
   await heading.click();
 
-  const fundSection = page.locator('div').filter({ hasText: /^Neon raising fund #1$/ });
+  const fundSection = page.locator("div").filter({ hasText: /^Neon Raising Fund$/ });
   await expect(fundSection).toBeVisible();
   await fundSection.click();
 });
