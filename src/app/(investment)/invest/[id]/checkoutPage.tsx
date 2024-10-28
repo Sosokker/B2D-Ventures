ce68ab6b2053f6a8eaeaf39832d67f933ea05380
sirin <sirinza29@gmail.com>
2024-10-28 21:06:12 +0700

    refactor: remove unused import

--- src/app/(investment)/invest/[id]/checkoutPage.tsx
+++ src/app/(investment)/invest/[id]/checkoutPage.tsx
@@ -14,7 +14,6 @@ import {
   DialogClose,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import useSession from "@/lib/supabase/useSession";
 import { createSupabaseClient } from "@/lib/supabase/clientComponentClient";
 import { useRouter } from "next/navigation";
 
@@ -37,9 +36,6 @@ const CheckoutPage = ({
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const isAcceptTerm = isAcceptTermAndService();
   const router = useRouter();
-
-  const { session } = useSession();
-  const user = session?.user;
 
   useEffect(() => {
     fetch("/api/create-payment-intent", {
@@ -108,7 +104,8 @@ const CheckoutPage = ({
       <div className="flex items-center justify-center">
         <div
           className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-          role="status">
+          role="status"
+        >
           <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
             Loading...
           </span>
@@ -130,7 +127,8 @@ const CheckoutPage = ({
             type="button"
             onClick={() => setIsDialogOpen(true)}
             disabled={!stripe || loading}
-            className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse">
+            className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
+          >
             {!loading ? `Pay $${amount}` : "Processing..."}
           </button>
         </DialogTrigger>
