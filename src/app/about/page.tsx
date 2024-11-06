7084d4d0aafc924be24bf285387eff3caa5482c8
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2024-11-06 14:41:37 +0700

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+export default async function About() {
+  return (
+    <div className="p-10">
+      <h1 className="mt-3 font-bold text-lg md:text-3xl">
+        About us
+      </h1>
+      <p className="p-5">
+        Welcome to B2D Ventures! We&apos;re a dynamic platform committed to bridging the gap
+        between visionary entrepreneurs and passionate investors. Our mission is to empower
+        innovation by connecting groundbreaking ideas with the resources they need to thrive.
+        Through B2D Ventures, we foster a community where investors and innovators come together
+        to transform concepts into impactful, real-world solutions.
+      </p>
+      <p className="p-5">
+        At B2D Ventures, we believe in the power of collaboration. Whether you&apos;re an investor
+        looking to support the next big idea or a founder aiming to bring your vision to life,
+        our platform offers the tools and connections to make it happen.
+        
+        Join us on a journey to reshape industries, drive positive change, and make a lasting impact.
+      </p>
+      <p className="p-5">
+        Let&apos;s build the future, together.
+      </p>
+    </div>
+  )
+}
