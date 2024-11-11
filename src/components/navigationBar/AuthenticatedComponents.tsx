72d2c8bbfec1d8ac5ae3bff2fef0d1499b92ed0c
Pattadon <pattadon2546pn@gmail.com>
2024-11-11 11:49:36 +0700

    feat: add CustomTooltip component and integrate it into navigation bar links

--- src/components/navigationBar/AuthenticatedComponents.tsx
+++ src/components/navigationBar/AuthenticatedComponents.tsx
@@ -13,6 +13,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Bell, Heart, Wallet, ChartPie } from "lucide-react";
 import { LogoutButton } from "@/components/auth/logoutButton";
 import { useUserRole } from "@/hooks/useUserRole";
+import CustomTooltip from "../customToolTip";
 
 interface AuthenticatedComponentsProps {
   uid: string;
@@ -29,24 +30,30 @@ export const AuthenticatedComponents = ({ uid, avatarUrl }: AuthenticatedCompone
 
   return (
     <div className={`flex gap-3 pl-2 items-center ${businessClass}`}>
-      <Link href={"/notification"}>
-        <div className="relative inline-block">
-          <Bell className="h-6 w-6 " />
-          <span className="absolute -top-1 -right-1 inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-600 rounded-full">
-            {displayValue}
-          </span>
-          <span className="absolute -top-1 -right-1 inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-600 rounded-full animate-ping"></span>
-        </div>
-      </Link>
+      <CustomTooltip message="Notification">
+        <Link href={"/notification"}>
+          <div className="relative inline-block">
+            <Bell className="h-6 w-6 " />
+            <span className="absolute -top-1 -right-1 inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-600 rounded-full">
+              {displayValue}
+            </span>
+            <span className="absolute -top-1 -right-1 inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-600 rounded-full animate-ping"></span>
+          </div>
+        </Link>
+      </CustomTooltip>
       <Heart />
-      <Link href={"/portfolio/" + uid}>
-        <Wallet className="cursor-pointer" />
-      </Link>
+      <CustomTooltip message="Portfolio">
+        <Link href={"/portfolio/" + uid}>
+          <Wallet className="cursor-pointer" />
+        </Link>
+      </CustomTooltip>
       {/*chart pie icon for bussiness's dashboard */}
       {data?.role === "business" && (
-        <Link href="/dashboard">
-          <ChartPie />
-        </Link>
+        <CustomTooltip message="Dashboard">
+          <Link href="/dashboard">
+            <ChartPie />
+          </Link>
+        </CustomTooltip>
       )}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
