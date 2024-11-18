46abff402dbca41bf63df6328de88405a17cae2b
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-11-18 15:19:03 +0700

    feat: enhance mobile menu with theme toggle and improve layout for better usability

--- src/components/navigationBar/mobileMenu.tsx
+++ src/components/navigationBar/mobileMenu.tsx
@@ -15,6 +15,7 @@ import React from "react";
 import { SearchBar } from "./serchBar";
 import ListItem from "../listItem";
 import { businessComponents, projectComponents, dataroomComponents } from "./menu";
+import { ThemeToggle } from "../theme-toggle";
 
 export function MobileMenu() {
   const [isVisible, setIsVisible] = useState(false);
@@ -29,11 +30,11 @@ export function MobileMenu() {
           animate={{ x: 0 }}
           exit={{ x: "-100%" }}
           transition={{ duration: 0.3 }}
-          className="fixed top-0 left-0 w-full bg-gray-800 text-white flex"
+          className="fixed top-0 left-0 w-full bg-blue-500  flex items-center"
         >
-          <X className="cursor-pointer w-10" onClick={() => setIsVisible(false)} />
+          <X className="cursor-pointer w-8" onClick={() => setIsVisible(false)} />
           <NavigationMenu>
-            <NavigationMenuList className="grid grid-cols-3">
+            <NavigationMenuList className="grid grid-cols-3 space-x-4 mt-1">
               <NavigationMenuItem>
                 <NavigationMenuTrigger className="text-base">Businesses</NavigationMenuTrigger>
                 <NavigationMenuContent>
@@ -45,7 +46,7 @@ export function MobileMenu() {
                 </NavigationMenuContent>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <NavigationMenuTrigger className="text-base font-medium ">Projects</NavigationMenuTrigger>
+                <NavigationMenuTrigger className="text-base ">Projects</NavigationMenuTrigger>
                 <NavigationMenuContent>
                   <ul className="grid w-[400px] ">
                     {projectComponents.map((component) => (
@@ -57,7 +58,7 @@ export function MobileMenu() {
                 </NavigationMenuContent>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <NavigationMenuTrigger className="text-base font-medium ">Dataroom</NavigationMenuTrigger>
+                <NavigationMenuTrigger className="text-base  ">Dataroom</NavigationMenuTrigger>
                 <NavigationMenuContent>
                   <ul className="grid w-[400px] ">
                     {dataroomComponents.map((component) => (
@@ -69,7 +70,8 @@ export function MobileMenu() {
                 </NavigationMenuContent>
               </NavigationMenuItem>
 
-              <NavigationMenuItem className="pl-5 flex">
+              <NavigationMenuItem className="pl-5 flex mt-5 flex">
+                <ThemeToggle />
                 <SearchBar />
               </NavigationMenuItem>
             </NavigationMenuList>
